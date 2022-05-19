1545402884be1efefb4c55be72bed4f05f3045ed
Hampus83 <jhmacgyver@gmail.com>
2022-05-19 16:06:47 +0200

    fixat createdAt

--- index.js
+++ index.js
@@ -4,48 +4,37 @@ const PORT = 8000;
 
 app.use(express.json());
 
-const todos = [
+let todos = [
     {
         todo: 'vakna',
         done: false,
-        // id: 1
     },
     {
         todo: 'ta på kläder',
         done: false,
-        // id: 2
     },
     {
         todo: 'gör kaffe',
         done: false,
-        // id: 3
     }
 ];
 
-fixID();
+let index = 1 
 
-function fixID() {
-    let index = 1 
-
-    todos.map((todo) => {
-        todo.id = index;
-        index ++;
-        todo.createdAt = Date();
-        return todo;
-    });
-    console.log('index(in fixID):', index);
-}
+todos.map((todo) => {
+    todo.id = index;
+    index ++;
+    return todo;
+});
 
 console.log('original todos:', todos);
 
 app.get('/api/todo', (request, response) => {
-    
-    fixID();
 
-    // todos.map((todo) => {
-    //     todo.createdAt = Date;
-    //     return todo;
-    // });
+    todos.map((todo) => {
+        todo.createdAt = Date();
+        return todo;
+    });
 
     response.json(todos);
     console.log('GET', todos)
@@ -53,9 +42,10 @@ app.get('/api/todo', (request, response) => {
 
 app.post('/api/todo', (request, response) => {
     const todo = request.body;
-    // todo.id = todos.length + 1;
+    todo.id = todos.length + 1;
+    todo.createdAt = Date();
     todos.push(todo);
-    fixID();
+
     response.json(todos);
     console.log('POST', todos);
 });
@@ -63,7 +53,7 @@ app.post('/api/todo', (request, response) => {
 app.delete('/api/todo/:id', (request, response) => {
     const chosenTodo = request.params.id;
 
-    const filteredTodos = todos.filter(todos => todos.id != chosenTodo)
+    let filteredTodos = todos.filter(todos => todos.id != chosenTodo)
     
     let index = 1; 
 
@@ -72,8 +62,11 @@ app.delete('/api/todo/:id', (request, response) => {
         index ++;
         return todo;
     });
-    response.json(filteredTodos);
-    console.log('DELETE:', filteredTodos);
+
+    todos = filteredTodos
+
+    response.json(todos);
+    console.log('DELETE:', todos);
 });
 
 app.listen(PORT, () => {
